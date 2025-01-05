747f137bc2603a6729925d13848bfdd0b9bc391b
Amir vTarikhi <85322640+eTarikhi@users.noreply.github.com>
2025-01-05 09:07:30 +0000

    04_01 Initialize React project with Vite setup, including basic configuration, components, and styles

---
+++ first-react-project/04/react-project/src/App.jsx
@@ -0,0 +1,58 @@
+// import React from "react";
+import "./App.css";
+import chef from "./images/chef.jpg";
+
+const items = [
+  "Macaroni and Cheese",
+  "Salmon with Potatoes",
+  "Tofu with Vegetables",
+];
+
+const dishObjects = items.map((dish, i) => ({
+  id: i,
+  title: dish,
+}));
+// console.log(dishObjects);
+
+function Header({ name, year }) {
+  // console.log(props);
+  return (
+    <div>
+      <h1>{name}'s Kitchen</h1>
+      <p>Copyright {year}</p>
+    </div>
+  );
+}
+
+function Main({ dishes }) {
+  return (
+    <>
+      <div>
+        <h2>Welcome to this beautiful restaurant!</h2>
+      </div>
+      <main>
+        <img src={chef} height={200} alt="A photo of a smiling chef owner" />
+        <ul>
+          {dishes.map((dish) => (
+            <li key={dish.id} style={{ listStyleType: "none" }}>
+              {dish.title}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header name="Emir" year="2023" />
+      <Main dishes={dishObjects} />
+      <Header name="Amir" year="2024" />
+      <Header name="Ali" year={new Date().getFullYear()} />
+    </div>
+  );
+}
+
+export default App;
