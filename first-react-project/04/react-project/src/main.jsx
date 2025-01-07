fffbf929914399a15f132cd9624f7ba448fa9ecd
Amir vTarikhi <85322640+eTarikhi@users.noreply.github.com>
2025-01-07 08:23:08 +0000

    04_01 Refactor main.jsx to use consistent quotation marks and add console log for variable demonstration

--- first-react-project/04/react-project/src/main.jsx
+++ first-react-project/04/react-project/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+const [first, second, third] = ["giraffe", "cat", "dog"];
+console.log(first, second, third);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
