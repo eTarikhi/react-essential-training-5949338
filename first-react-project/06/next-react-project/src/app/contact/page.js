7649c9c59c47b09bc4c3a2f35842c6fae8c75c74
Amir vTarikhi <85322640+eTarikhi@users.noreply.github.com>
2025-01-13 04:37:05 +0000

    06_04 Enhance Contact Us page with form for user input

--- first-react-project/06/next-react-project/src/app/contact/page.js
+++ first-react-project/06/next-react-project/src/app/contact/page.js
@@ -1,13 +1,47 @@
-
 export default function Page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold text-center sm:text-left">Contact Us</h1>
+    <div className="grid grid-rows items-center justify-items-center min-h-screen">
+      <main className="flex flex-col gap-8 row-start-2 items-center lg:items-start">
+        <h1 className="text-4xl font-bold text-center sm:text-left">
+          Contact Us
+        </h1>
+        <form className="flex flex-col gap-4 w-full">
+          <label htmlFor="name" className="flex flex-col gap-1">
+            <span className="text-sm font-bold">Name</span>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="p-2 border border-gray-300 rounded"
+              required
+            />
+          </label>
+          <label htmlFor="email" className="flex flex-col gap-1">
+            <span className="text-sm font-bold">Email</span>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="p-2 border border-gray-300 rounded"
+              required
+            />
+          </label>
+          <label htmlFor="message" className="flex flex-col gap-1">
+            <span className="text-sm font-bold">Message</span>
+            <textarea
+              name="message"
+              id="message"
+              className="p-2 border border-gray-300 rounded"
+              row="4"
+              required
+            />
+          </label>
+          <button type="submit" className="bg-blue-500 text-white font-bold py-2 rounded hover:bg-blue-600">
+            Send
+          </button>
+        </form>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-
-      </footer>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
